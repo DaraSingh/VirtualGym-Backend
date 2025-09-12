39b1d1ddb72693837b80a5d99126d8c7f69f041c
DaraSingh <desd2001@gmail.com>
2025-09-12 14:32:12 +0530

    encrypted password and enabled uniqueness

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-const express=require('express');
-const cors=require('cors')
-const db=require("./config/mongoose-connection")
-const app=express();
-const userModel=require("./models/userModel")
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+const db = require("./config/mongoose-connection");
+const app = express();
+const userModel = require("./models/userModel");
+const bcrypt = require("bcrypt");
 
-app.post("/register",async(req,res)=>{
-    const {name,email,password}=req.body;
-    const createdUser=await userModel.create({
-        name,
-        email,
-        password
-    })
-    res.json(createdUser)
-})
-
-app.get("/listUsers",async(req,res)=>{
-    const users=await userModel.find({})
-    res.send(users)
-})
-
-app.get("/deleteUsers",async(req,res)=>{
-    const users=await userModel.deleteMany({})
-    res.send(users)
-})
+app.use(cors());
+app.use(express.json());
 
 
 
-app.listen(3000,
-    console.log("listening at port 3000")
-)
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const duplicate = await userModel.findOne({ email: email });
+    if (duplicate)
+      return res.status(400).json({ message: "Email already exists" });
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        const createdUser = await userModel.create({
+          name,
+          email,
+          password: hash,
+        });
+        res.json({ message: "User Created Successfully" });
+      });
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+app.get("/listUsers", async (req, res) => {
+  const users = await userModel.find({});
+  res.send(users);
+});
+
+app.get("/deleteUsers", async (req, res) => {
+  const users = await userModel.deleteMany({});
+  res.send(users);
+});
+
+app.listen(3000, console.log("listening at port 3000"));
